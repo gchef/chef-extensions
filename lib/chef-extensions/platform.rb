786dbb8aba81ba7567cf75d37cebce0380c0d24d
Gerhard Lazu <gerhard@lazu.co.uk>
2012-02-13 17:29:32 +0000

    Testing docs

--- lib/chef-extensions/platform.rb
+++ lib/chef-extensions/platform.rb
@@ -3,10 +3,14 @@ class Chef
     module Platform
       extend self
 
+      # Returns true if running on OS X
+      #
       def os_x?
         RUBY_PLATFORM.index('darwin')
       end
 
+      # Returns true if running on Linux
+      #
       def linux?
         RUBY_PLATFORM.index('linux')
       end
