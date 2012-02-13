6b993629ea6d4f8ac36357da5597061f73233ded
Gerhard Lazu <gerhard@lazu.co.uk>
2012-02-13 13:11:49 +0000

    Updated docs

--- lib/chef-extensions/ec2.rb
+++ lib/chef-extensions/ec2.rb
@@ -3,7 +3,7 @@ class Chef
     module EC2
       extend self
 
-      # **Get the instance ID from AWS's metadata service**
+      # **Get the instance ID from AWS's metadata service** <br />
       # If we don't hear back within 1 second, time out.
       def instance_id
         `curl --connect-timeout 1 http://169.254.169.254/2011-01-01/meta-data/instance-id 2>&1`.chomp
