5db4430364231c38f0b3246c6f00c763895c55c0
Gerhard Lazu <gerhard@lazu.co.uk>
2012-03-27 17:37:39 +0100

    Architecture support 64bit, 32bit

--- lib/chef-extensions/platform.rb
+++ lib/chef-extensions/platform.rb
@@ -14,6 +14,30 @@ class Chef
       def linux?
         RUBY_PLATFORM.index('linux')
       end
+
+      # Returns true if architecture is 64bit!
+      #
+      def amd64?
+        !! architecture.index("x86_64")
+      end
+
+      # Returns true if architecture is 32bit!
+      #
+      def i386?
+        ! amd64?
+      end
+
+      # Package friendly architecture string
+      #
+      def arch
+        amd64? ? "amd64" : "i386"
+      end
+
+      # Retrieves and caches the architecture type
+      #
+      def architecture
+        @architecture ||= `uname -m`
+      end
     end
   end
 end
---
+++ test/chef-extensions/platform_test.rb
@@ -0,0 +1,44 @@
+require File.expand_path('../../test_helper', __FILE__)
+require 'chef-extensions/platform'
+
+class Chef
+  module Extensions
+    describe Platform do
+      describe "32bit architectures" do
+        before do
+          Platform.instance_variable_set(:@architecture, "i686")
+        end
+
+        it "amd64?" do
+          Platform.amd64?.must_equal false
+        end
+
+        it "i386?" do
+          Platform.i386?.must_equal true
+        end
+
+        it "arch" do
+          Platform.arch.must_equal "i386"
+        end
+      end
+
+      describe "64bit architectures" do
+        before do
+          Platform.instance_variable_set(:@architecture, "x86_64")
+        end
+
+        it "amd64?" do
+          Platform.amd64?.must_equal true
+        end
+
+        it "i386?" do
+          Platform.i386?.must_equal false
+        end
+
+        it "arch" do
+          Platform.arch.must_equal "amd64"
+        end
+      end
+    end
+  end
+end
